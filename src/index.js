702ce429330571dc83c458b7a8ff0b02968d0cc3
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-12 18:31:26 +0300

    Create array of objects of to do tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import _ from "lodash";
 import "./style.css";
+
+const todoTasks = [
+  {
+    index: 0,
+    description: "washing the dishes",
+    completed: true,
+  },
+  {
+    index: 1,
+    description: "complete To Do list project",
+    completed: true,
+  },
+  {
+    index: 1,
+    description: "fix car",
+    completed: true,
+  },
+];

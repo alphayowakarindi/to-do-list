a42a8d9527d85ce1e14e44ce1782afc0dc68a3c2
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-12 21:44:55 +0300

    Extract the array of todo tasks to a module

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import './style.css';
+import { todoTasks } from '../modules/todos.js';
 
 // Selectors
 const todoListUl = document.querySelector('.todo-list');
-
-const todoTasks = [
-  {
-    index: 0,
-    description: 'washing the dishes',
-    completed: true,
-  },
-  {
-    index: 1,
-    description: 'complete To Do list project',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'fix car',
-    completed: true,
-  },
-];
 
 todoTasks.forEach((todoTask) => {
   todoListUl.innerHTML += `

7d848fa680fee3c948378ef7e41156a89f3bcbda
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-12 20:42:42 +0300

    Remove unused lodash import to fix linters error

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import _ from "lodash";
-import "./style.css";
+import './style.css';
 
 // Selectors
-const todoListUl = document.querySelector(".todo-list");
+const todoListUl = document.querySelector('.todo-list');
 
 const todoTasks = [
   {
     index: 0,
-    description: "washing the dishes",
+    description: 'washing the dishes',
     completed: true,
   },
   {
     index: 1,
-    description: "complete To Do list project",
+    description: 'complete To Do list project',
     completed: true,
   },
   {
     index: 1,
-    description: "fix car",
+    description: 'fix car',
     completed: true,
   },
 ];

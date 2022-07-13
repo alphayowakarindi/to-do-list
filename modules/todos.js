e77eb0b87a3763bc37d2ee17bf5906758ea39fe7
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-13 12:50:03 +0300

    Validate and grab description value on form submit

--- modules/todos.js
+++ modules/todos.js
@@ -4,3 +4,9 @@ const addTaskForm = document.querySelector('#add-task-form')
 
 export const todoTasks = [];
 
+// Event listners
+addTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const description = document.querySelector('#description').value;
+  if(!description) return
+})

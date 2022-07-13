e63ce80c799aaf13819db91ec725d168ec92aac3
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-13 12:53:05 +0300

    Create book class

--- modules/todos.js
+++ modules/todos.js
@@ -1,12 +1,21 @@
 /* eslint-disable import/prefer-default-export */
 // Selectors
-const addTaskForm = document.querySelector('#add-task-form')
+const addTaskForm = document.querySelector('#add-task-form');
 
 export const todoTasks = [];
 
+// Classes
+class Book {
+  constructor(description) {
+    this.index = todoTasks.length;
+    this.completed = false;
+    this.description = description;
+  }
+}
+
 // Event listners
 addTaskForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+  e.preventDefault();
   const description = document.querySelector('#description').value;
-  if(!description) return
-})
+  if (!description) return;
+});

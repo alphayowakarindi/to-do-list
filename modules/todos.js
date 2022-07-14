482f894572479752e20e7d1b9790273ffd9d8b62
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-14 13:06:36 +0300

    Add data attribute to input with input-with-task class

--- modules/todos.js
+++ modules/todos.js
@@ -49,7 +49,7 @@ export class UI {
     <form id="update-form">
               <li>
                   <input type="checkbox">
-                  <input class="input-with-task"  disabled value="${task.description}" type="text" id="input-with-task"/>
+                  <input class="input-with-task"  disabled value="${task.description}" type="text" id="input-with-task" data-index="${task.index}"/>
                   <i class="fa-solid fa-ellipsis-vertical edit-task" data-index="${task.index}"></i>
                   <i class="fa-solid fa-trash-can" data-index="${task.index}"></i>
               </li>  

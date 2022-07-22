e6a9aef40ace512b130730fa8b2c79e968eaa10a
George D. Kruah <dfizzkruah@gmail.com>
2022-07-22 18:26:41 +0300

    Update modules file in JS to test removal of elements from the Dom

--- modules/store.test.js
+++ modules/store.test.js
@@ -18,4 +18,30 @@ describe("add or removing task", () => {
     const todoTasks = JSON.parse(localStorage.getItem("todoTasks"));
     expect(Store.remove(3)).toEqual(todoTasks[2]);
   });
+
+  test("Remove one new item to the list", () => {
+    document.body.innerHTML = `
+    <section id="todo-list">
+    <div class="title">
+      <h1>Today's To Do</h1>
+      <i class="fa-solid fa-arrows-rotate"></i>
+    </div>
+    <hr />
+    <form id="add-task-form">
+      <input type="text" placeholder="Add to your list..." id="description"/>
+      <button id="submit" type="submit" hidden></button>
+    </form>
+    <hr />
+    <ul class="todo-list"></ul>
+    <div class="clear-btn">
+      <span>Clear all completed</span>
+    </div>
+  </section>
+    `;
+
+    const todoListUl = document.querySelector('.todo-list');
+    UI.addTaskToList('heloo')
+    expect(todoListUl.childNodes.length).toBe(4);
+  });
+
 });
